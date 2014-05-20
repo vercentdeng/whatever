372ab5c7bf162dfffaaf16b758c367fbae057277
SereneSky <vercent@170.253.229.114.broad.zj.js.dynamic.163data.com.cn>
2014-05-20 19:21:08 +0800

    Ð޸ÄÁËadmin/index.php

--- admin/index.php
+++ admin/index.php
@@ -91,17 +91,6 @@ $stat = Typecho_Widget::widget('Widget_Stat');
                     </ul>
                 </section>
             </div>
-
-            <div class="col-mb-12 col-tb-4" role="complementary">
-                <section class="latest-link">
-                    <h3><?php _e('官方最新日志'); ?></h3>
-                    <div id="typecho-message">
-                        <ul>
-                            <li><?php _e('读取中...'); ?></li>
-                        </ul>
-                    </div>
-                </section>
-            </div>
         </div>
     </div>
 </div>
@@ -111,48 +100,4 @@ include 'copyright.php';
 include 'common-js.php';
 ?>
 
-<script>
-$(document).ready(function () {
-    var ul = $('#typecho-message ul'), cache = window.sessionStorage,
-        html = cache ? cache.getItem('feed') : '',
-        update = cache ? cache.getItem('update') : '';
-
-    if (!!html) {
-        ul.html(html);
-    } else {
-        html = '';
-        $.get('<?php $options->index('/action/ajax?do=feed'); ?>', function (o) {
-            for (var i = 0; i < o.length; i ++) {
-                var item = o[i];
-                html += '<li><span>' + item.date + '</span> <a href="' + item.link + '" target="_blank">' + item.title 
-                    + '</a></li>';
-            }
-
-            ul.html(html);
-            cache.setItem('feed', html);
-        }, 'json');
-    }
-
-    function applyUpdate(update) {
-        if (update.available) {
-            $('<div class="update-check"><p>' 
-                + '<?php _e('您当前使用的版本是 %s'); ?>'.replace('%s', update.current) + '<br />'
-                + '<strong><a href="' + update.link + '" target="_blank">' 
-                + '<?php _e('官方最新版本是 %s'); ?>'.replace('%s', update.latest) + '</a></strong></p></div>')
-            .appendTo('.welcome-board').effect('highlight');
-        }
-    }
-
-    if (!!update) {
-        applyUpdate($.parseJSON(update));
-    } else {
-        update = '';
-        $.get('<?php $options->index('/action/ajax?do=checkVersion'); ?>', function (o, status, resp) {
-            applyUpdate(o);
-            cache.setItem('update', resp.responseText);
-        }, 'json');
-    }
-});
-
-</script>
 <?php include 'footer.php'; ?>
